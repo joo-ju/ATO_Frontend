3835cdfeab9e8ec80c2e5d122632c1cdef35261b
김주희 <rlawn97@skuniv.ac.kr>
2021-09-08 11:59:30 +0900

    Initial Commit

---
+++ ato_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ato_ios
+//
+//  Created by 김주희 on 2021/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ato_ios/ato_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ato_iosApp.swift
+//  ato_ios
+//
+//  Created by 김주희 on 2021/09/08.
+//
+
+import SwiftUI
+
+@main
+struct ato_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
